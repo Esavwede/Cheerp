8491b6d13557f7d380f4092e53ad4f8b22c42017
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-07-04 14:25:02 +0100

    First commit

--- src/routes/User/user.route.js
+++ src/routes/User/user.route.js
@@ -3,19 +3,21 @@ const router = express.Router()
 const user = require('../../controller/User/user.controller') 
 
 
-module.exports = function(app)
-    {
-        try 
-        {
-            router.post('/', user.create )
-            app.use('/user', router )
+// module.exports = function(app)
+//     {
+//         try 
+//         {
+//             router.post('/', user.create )
+//             app.use('/user', router )
             
-            console.log(' User routes created ')
-        }
-        catch(e)
-        {
-            console.log(' Error occured while creating user routes ')
-            console.log(e) 
-        }
-    }
+//             console.log(' User routes created ')
+//         }
+//         catch(e)
+//         {
+//             console.log(' Error occured while creating user routes ')
+//             console.log(e) 
+//         }
+//     }
+
+router.
 
